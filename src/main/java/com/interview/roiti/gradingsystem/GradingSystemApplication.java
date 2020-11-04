21084e0454ed6b36334f05c7e97f87d143034e1f
Vesela Bandova <v.bandova@gmail.com>
2020-11-04 22:52:27 +0200

    Initial commit

---
+++ src/main/java/com/interview/roiti/gradingsystem/GradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.interview.roiti.gradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingSystemApplication.class, args);
+	}
+
+}
